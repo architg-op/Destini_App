932aa42be5e5e9939a0d52b6f584faeb1804a3d0
Archit Garg <archit.g1998@gmail.com>
2020-01-27 21:52:59 +0530

    Choice Destinations linked (Still Incomplete)

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
+    var storyNumber = 0
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.stories[storyNumber].title
+        choice1Button.setTitle(storyBrain.stories[storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[storyNumber].choice2, for: .normal)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
-
-
+    
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyNumber = storyBrain.nextStory(userChoice: sender.currentTitle!, storyNumber : storyNumber)
+        updateUI()
+    }
+    
+    
+    
+    
 }
 
